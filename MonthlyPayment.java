9064259987e0d8f29e24f02371e27e123c63250c
billqmao <31422272+billqmao@users.noreply.github.com>
2017-09-25 18:17:25 -0400

    Version 1.15
    Finished up the monthly payment class

--- MonthlyPayment.java
+++ MonthlyPayment.java
@@ -5,13 +5,15 @@ import java.io.*;
  * Date: 9/25/17
  * School: East Chapel Hill High School
  * Program: Program 58i (monthly payment)
- * Description: This program is designed to ask the user for a baseball player's name, adn three statistics related to baseball:...
- * the times at bat, number of hits, and number of walks for the player.
- * It then takes these statistics and calculates the batting average and on base percentage.
- * The batting average and on base percentage are then rounded to three decimal points, and are printed for the user to see.
- * Difficulties: When I created this program, I first rounded incorrectly by adding .05 instead of .5.
- * What I Learned: In this exercise, I got some more practice with casting variables and getting user input with the scanner class.
- * I also better understand how the rounding calculation works now.
+ * Description: This program is designed to first store three details from a car loan:...
+ * the amount borrowed, the interest rate, and the amount of years it will take to pay that loan.
+ * The program then takes these values and finds the monthly payment for the car, the total amount paid to the bank, and the amount of interest paid.
+ * It then prints these results for the user to see.
+ * Difficulties: I split the monthly payment calculation into two parts in order to make it more readable,...
+ * as I tried to do it in one line of code, but was confused by all of the parentheses.
+ * I also mixed up two variables leading to a calculation error.
+ * What I Learned: In this exercise, I got some more practice with casting variables and using the scanner object for user input
+ * I also got some practice with rounding and the Math class.
  */
 
 public class MonthlyPayment
@@ -19,41 +21,66 @@ public class MonthlyPayment
     public static void main(String args[])
     {
         Scanner keyboardInput = new Scanner(System.in); //Creating the scanner object in order to use it
-        //Asks for the Player's name, waits for input, and then stores it as one string
+        //Asks for the principal of the loan, waits, and then stores the response as a 64 bit floating point
         System.out.println("The amount you wish to borrow is:_");
         double amtBorrowed = keyboardInput.nextDouble();
-        //Asks for statistic related to batting average, waits for input, and then stores that variable as an integer
+        //Asks for the interest rate as a percentage, converts it to a decimal, and stores it as a double precision floating point
         System.out.println("The loan rate you can get as a percentage:_");
         double intRate = .01 * keyboardInput.nextDouble();
-        //
+        //Asks for the number of years the loan will take to pay off and stores it as an integer
         System.out.println("The number of years you will take to pay off the loan:_");
         int yearsPaying = keyboardInput.nextInt();
-        //
+        //The amount of year that it will take to pay off the debt is first converted to months
         int monthsPaying = yearsPaying * 12;
+        // The monthly interest rate is then calculated
         double monthlyInt = Math.pow(((intRate / 12) + 1), monthsPaying);
-        double payMonth =  amtBorrowed * (monthlyInt / 12) * (monthlyInt / (monthlyInt - 1));
+        //It is then used to calculate the monthly payment for the car
+        double payMonth =  amtBorrowed * (intRate / 12) * (monthlyInt / (monthlyInt - 1));
+        //The monthly payment is then used to calculate the total payment, which is the  used to find the amount of interest paid
         double totalPay =  payMonth * monthsPaying;
         double intPaid = totalPay - amtBorrowed;
-        //Rounds the on base percentage and batting average to the thousandths place
+        //The Monthly payment, total payment, and interest paid are then rounded to two decimal places
         double payMonthRound = (int) (payMonth * 100 + .5) / 100.0;
         double totalPayRound = (int) (totalPay * 100 + .5) / 100.0;
         double intPaidRound = (int) (intPaid * 100 +.5) / 100.0;
-        //Prints the results of the calculations
+        //All three rounded values are then printed for the user to see
         System.out.println("My monthly payments will be $" + payMonthRound);
         System.out.println("Total interest paid is $" + intPaidRound );
         System.out.println("Total amount paid is $" + totalPayRound);
     }
 }
 
-//Example output of a player named John Doe with 100 times at bat, 20 hits, and five walks
-//Enter Player's Name:_
-//John Doe
-//Enter Number of Times at bat:_
-//100
-//Enter Number of Hits:_
-//20
-//Enter Number of Walks:_
-//5
-//John Doe has a batting average of 0.211 and an On Base Percentage of 0.25
+//Example output with the three scenarios in the document
+/*
+ * The amount you wish to borrow is:_
+ * 7500
+ * The loan rate you can get as a percentage:_
+ * 14.5
+ * The number of years you will take to pay off the loan:_
+ * 3
+ * My monthly payments will be $258.16
+ * Total interest paid is $1793.66
+ * Total amount paid is $9293.66
+ *
+ * The amount you wish to borrow is:_
+ * 7500
+ * The loan rate you can get as a percentage:_
+ * 14.5
+ * The number of years you will take to pay off the loan:_
+ * 4
+ * My monthly payments will be $206.83
+ * Total interest paid is $2428.06
+ * Total amount paid is $9928.06
+ *
+ * The amount you wish to borrow is:_
+ * 7500
+ * The loan rate you can get as a percentage:_
+ * 7.5
+ * The number of years you will take to pay off the loan:_
+ * 3
+ * My monthly payments will be $233.3
+ * Total interest paid is $898.68
+ * Total amount paid is $8398.68
+ */
 
 
