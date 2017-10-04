7865de1b383157034792f4a7e985c95bfebc2374
billqmao <31422272+billqmao@users.noreply.github.com>
2017-10-04 12:09:18 -0400

    Version 1.31
    Finished up some of the new projects

--- hourlyPay.java
+++ hourlyPay.java
@@ -5,19 +5,67 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+
 import java.util.*;
 import java.io.*;
+
 public class hourlyPay
 {
     public static void main(String args[])
     {
         Scanner kbInput = new Scanner(System.in); //Creates a new scanner object in order to use user input
-        System.out.println("Hours" + "\t" + "Pay");
+        System.out.println("Hours" + "\t" + "Pay"); //Prints ot the Hours and Pay part of the table.
         int pay;
         for(int hours = 1; hours <= 40; hours++)
         {
             pay = hours * 4;
-            System.out.println(hours + "\t" + pay);
+            System.out.println(" " + hours + "\t " + pay);
         }
     }
 }
+
+//Output:
+//Note: The formatting was changed when copying from the terminal...,
+//so this output may appear to be formatted incorrectly.
+//Hours	Pay
+// 1	 4
+// 2	 8
+// 3	 12
+// 4	 16
+// 5	 20
+// 6	 24
+// 7	 28
+// 8	 32
+// 9	 36
+// 10	 40
+// 11	 44
+// 12	 48
+// 13	 52
+// 14	 56
+// 15	 60
+// 16	 64
+// 17	 68
+// 18	 72
+// 19	 76
+// 20	 80
+// 21	 84
+// 22	 88
+// 23	 92
+// 24	 96
+// 25	 100
+// 26	 104
+// 27	 108
+// 28	 112
+// 29	 116
+// 30	 120
+// 31	 124
+// 32	 128
+// 33	 132
+// 34	 136
+// 35	 140
+// 36	 144
+// 37	 148
+// 38	 152
+// 39	 156
+// 40	 160
+
--- straightLine.java
+++ straightLine.java
@@ -1,15 +1,27 @@
 /**
  * Name: Bill Mao
+ * 
  * Date: 10/2/17
+ * 
  * School: East Chapel Hill High School
- * Program: Program 65a (International Date)
- * Description: This program takes a date in American format, and converts it to the ISO standard.
- * It then prints the ISO standard out.
+ * 
+ * Program: Program 108b (straight line)
+ * 
+ * Description: This program is designed to calculate the annual depreciation of a car 4 times.
+ * A for loop is first set up to repeat the calculation 4 times, and the run number is printed for the user to see each time.
+ * The calculation begins by asking the user 3 variables needed to find the depreciation:...
+ * The price of the car, the salvage value, and the estimated life in years.
+ * These values are then stored and used to find the annual depreciation.
+ * The annual depreciation is then rounded to two decimal places, and is printed for the user to see.
+ * 
  * Difficulties: I originally got confused as to where semicolons should be put in the for statement.
  * Once I looked over my notes and rewatched some of the videos, I managed to correct this error.
  * I also got the backslash and forward slash mixed up one for the \n.
- * What I Learned: I got some more practice with using the scanner class to get input.
+ * 
+ * What I Learned: I learned how to use the for loop in this exercise. 
+ * I also got some more practice with casting, mathematical operations, and the scanner class.
  */
+
 import java.util.*;
 import java.io.*;
 
@@ -18,17 +30,60 @@ public class straightLine
     public static void main(String args[])
     {
         Scanner kbInput = new Scanner(System.in); //Creates a new scanner object in order to use user input
-        for(int runNum = 1; runNum <= 4; runNum++)
+
+        for(int runNum = 1; runNum <= 4; runNum++) //Repeats the code 4 times
         {
+            //Asks user for price of the car, salvage value, and estimated life and stores them
+            //Also prints the run number out
+            System.out.println("Run#" + runNum);
             System.out.println("Enter Price:");
             double price = kbInput.nextDouble();
             System.out.println("Enter Salvage Value:");
             double salvage = kbInput.nextDouble();
             System.out.println("Enter Estimated Life in Years:");
             int years = kbInput.nextInt();
+            //Calculates the annual depreciation using the three values, and rounds it to the hundredths place
             double depreciation = (price - salvage) / years;
             double depreciationRound = (int) (depreciation * 100 + .5) / 100.0;
+            //Prints the results of the calculation
             System.out.println("Annual Depreciation = " + depreciationRound + "\n");
         }
     }
 }
+
+//Output:
+//Run#1
+//Enter Price:
+//250
+//Enter Salvage Value:
+//35
+//Enter Estimated Life in Years:
+//8
+//Annual Depreciation = 26.88
+//
+//Run#2
+//Enter Price:
+//2425
+//Enter Salvage Value:
+//470
+//Enter Estimated Life in Years:
+//6
+//Annual Depreciation = 325.83
+//
+//Run#3
+//Enter Price:
+//1162
+//Enter Salvage Value:
+//625
+//Enter Estimated Life in Years:
+//5
+//Annual Depreciation = 107.4
+//
+//Run#4
+//Enter Price:
+//9654.33
+//Enter Salvage Value:
+//100
+//Enter Estimated Life in Years:
+//12
+//Annual Depreciation = 796.19
