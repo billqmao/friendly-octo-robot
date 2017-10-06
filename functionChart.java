631dde0a01ff8bb56f9b7e60e3a1705a92dbe9e9
billqmao <31422272+billqmao@users.noreply.github.com>
2017-10-06 11:56:05 -0400

    Version 1.34
    Finished up the two classes previously introduced

--- functionChart.java
+++ functionChart.java
@@ -5,16 +5,18 @@
  *
  * School: East Chapel Hill High School
  *
- * Program: Program 122a (square vs squared)
+ * Program: Program 122c (function chart)
  *
- * Description: This program is designed to calculate the pay a person earns at $4 per hour in a 40 hour workweek.
- * The program firsts starts by printing the header of the table containing Hours and Pay, and initializes the pay variable.
- * It then executes a for loops that repeats 40 times for each hour worked.
- * In the loop, the pay for the cumulative amount of hours is calculated and is printed along with the number of hours worked.
+ * Description: This program is designed to find all even numbers between 0 and 50 inclusive, and print them...
+ * along with the even number plus one, multiplied by two, and squared.
+ * The program first enters into a for loop that loops 25 times for each even number and increments by two.
+ * In the loop, the program then finds the three modifications of the even number listed above.
+ * The even number and even+1, even*2, and even^2 are then printed in four columns.
+ * Again, this is repeated 25 times to create the table.
  *
- * Difficulties: I had some trouble formatting the square root values to be spaced correctly and to also print to four decimal places.
+ * Difficulties: I had some trouble with using the EasyFormat class and how much spacing to use.
  *
- * What I Learned: I got some more practice with formatting text in Java, the for loop, and mathematical operations.
+ * What I Learned: I got some more practice with the EasyFormat class, and mathematical operations.
  */
 
 import java.util.*;
@@ -24,17 +26,67 @@ public class functionChart
 {
     public static void main(String args[])
     {
-        //Loops the function 40 times for each hour worked
+        //Loops the function 25 times for even numbers between 1 and 50
         for(int even = 0; even <= 50; even += 2)
         {
-            int column2 = even +1;
-            int column3 = 2 * even;
-            int column4 = (int) Math.pow(even, 2);
+            int column2 = even +1; //Finds even number + 1 for the second column
+            int column3 = 2 * even; //Finds 2 times even number for column three
+            int column4 = (int) Math.pow(even, 2); //Squares even number
+            //Prints the even number, even + 1, even * 2, and even squared in four columns
             System.out.println(EasyFormat.format(even, 0) + EasyFormat.format(column2, 12) + EasyFormat.format(column3, 12) + EasyFormat.format(column4, 12) + "\n");
-            if(even > 56)
-            {
-                break;
-            }
         }
     }
 }
+
+//Output:
+//0           1           0           0
+
+//2           3           4           4
+
+//4           5           8          16
+
+//6           7          12          36
+
+//8           9          16          64
+
+//10          11          20         100
+
+//12          13          24         144
+
+//14          15          28         196
+
+//16          17          32         256
+
+//18          19          36         324
+
+//20          21          40         400
+
+//22          23          44         484
+
+//24          25          48         576
+
+//26          27          52         676
+
+//28          29          56         784
+
+//30          31          60         900
+
+//32          33          64        1024
+
+//34          35          68        1156
+
+//36          37          72        1296
+
+//38          39          76        1444
+
+//40          41          80        1600
+
+//42          43          84        1764
+
+//44          45          88        1936
+
+//46          47          92        2116
+
+//48          49          96        2304
+
+//50          51         100        2500
--- squareVsSquared.java
+++ squareVsSquared.java
@@ -7,14 +7,14 @@
  *
  * Program: Program 122a (square vs squared)
  *
- * Description: This program is designed to calculate the pay a person earns at $4 per hour in a 40 hour workweek.
- * The program firsts starts by printing the header of the table containing Hours and Pay, and initializes the pay variable.
- * It then executes a for loops that repeats 40 times for each hour worked.
- * In the loop, the pay for the cumulative amount of hours is calculated and is printed along with the number of hours worked.
+ * Description: This program is designed to print a table of the numbers 1-40, their squares, and their square roots in three columns.
+ * The program firsts starts by printing the heading of the table containing Number, Square and Square Root.
+ * A for loop in the program that loops 40 times from numbers 1 to 40 is then executed.
+ * In the loop, the number of the loop is squared and square rooted separately, and is then printed with the loop number in the table format.
  *
  * Difficulties: I had some trouble formatting the square root values to be spaced correctly and to also print to four decimal places.
  *
- * What I Learned: I got some more practice with formatting text in Java, the for loop, and mathematical operations.
+ * What I Learned: I got some more practice with the for loop, the Math class in Java, and use of the EasyFormat class.
  */
 
 import java.util.*;
@@ -24,13 +24,59 @@ public class squareVsSquared
 {
     public static void main(String args[])
     {
-        System.out.println("Hours\t" + "Square\t" + "Square Root"); //Prints ot the Hours and Pay part of the table.
-        //Loops the function 40 times for each hour worked
+        System.out.println("Number\t" + "Square\t" + "Square Root"); //Prints the header of the table
+        //Loops the function 40 times for number 1-40 inclusive
         for(int num = 1; num <= 40; num++)
         {
-            int numsquared = (int) Math.pow(num, 2);
-            double numsquarert = Math.sqrt(num);
+            int numsquared = (int) Math.pow(num, 2); //Squares the number for the second column
+            double numsquarert = Math.sqrt(num); //Square roots the number for the third column
+            //Prints the numbers 1-40, the square of the number, and the square root
             System.out.println(num + " \t" + numsquared + "\t   " + EasyFormat.format(numsquarert, 0, 4));
         }
     }
 }
+
+//Output:
+//Number    Square  Square Root
+//1     1      1.0000
+//2     4      1.4142
+//3     9      1.7321
+//4     16     2.0000
+//5     25     2.2361
+//6     36     2.4495
+//7     49     2.6458
+//8     64     2.8284
+//9     81     3.0000
+//10    100    3.1623
+//11    121    3.3166
+//12    144    3.4641
+//13    169    3.6056
+//14    196    3.7417
+//15    225    3.8730
+//16    256    4.0000
+//17    289    4.1231
+//18    324    4.2426
+//19    361    4.3589
+//20    400    4.4721
+//21    441    4.5826
+//22    484    4.6904
+//23    529    4.7958
+//24    576    4.8990
+//25    625    5.0000
+//26    676    5.0990
+//27    729    5.1962
+//28    784    5.2915
+//29    841    5.3852
+//30    900    5.4772
+//31    961    5.5678
+//32    1024   5.6569
+//33    1089   5.7446
+//34    1156   5.8310
+//35    1225   5.9161
+//36    1296   6.0000
+//37    1369   6.0828
+//38    1444   6.1644
+//39    1521   6.2450
+//40    1600   6.3246
+
+
