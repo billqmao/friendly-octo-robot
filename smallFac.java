340785f74551e34db27f5276a9bd7707b7c22512
billqmao <31422272+billqmao@users.noreply.github.com>
2017-10-11 12:26:36 -0400

    Version 1.44
    BUGFIXING NEEDED.

--- smallFac.java
+++ smallFac.java
@@ -5,21 +5,23 @@
  *
  * School: East Chapel Hill High School
  *
- * Program: Program 152d (summing some numbers)
+ * Program: Program 162a (small factorials)
  *
- * Description: This program is designed to find the integers between two numbers, its sum, and the average of those numbers.
- * It begins by asking for the user to provide the start and ending number, and stores those values.
- * It then starts the calculation by setting the sum to the starting value and printing the starting value, as the loop does not include this value.
- * It also initializes a count value that is set to and used in lieu of the starting value in the loop calculation...
- * in order to preserve the original starting value for future calculations
- * The loop then prints out the numbers between the two numbers given and adds them to the sum for each loop iteration.
- * The average of the numbers is then calculated and rounded.
- * The starting and ending numbers, sum and rounded average are then printed for the user to see.
+ * Description: This program is designed to find the factoralized number that the user provides.
+ * The program first begins by initializing and declaring all variables neede in the progrma so that they can be used...
+ * throughout the program.
+ * The first loop then starts by Asking the user for the number they want to factorialize.
+ * The number is the assigned to a different variable that is used in a for loop that multiplies...
+ * the starting number with the integers less than it until it reaches zero.
+ * The factorial calculation isthen printed for the user to see.
+ * The program then asks the user if they want to repeat the calcuation, and sets a boolean to true if the answer is yes.
+ * The boolean repeats the calculation contained in a do..while loop if it is true.
+ * The program ends and prints out an ending rsponse if the user answeres anything other than yes.
  *
- * Difficulties: I had some trouble getting the average to find the amount of numbers listed, but I eventually realised that you...
- * had to add one to the end minus start to account for the starting number
+ * Difficulties: I had some difficulty making the if statement case insensitive,...
+ * but I looked over my notes and eventually found the toLowerCase modifier.
  *
- * What I Learned: I learned how to use the do while loop, and I got some more practice with casting and mathematical operations in Java.
+ * What I Learned: I learned how to use nested loops in Java, and I got some more practice with loops and user input in general.
  */
 
 import java.util.*;
@@ -41,20 +43,29 @@ public class smallFac{
             number = kbInput.nextInt();
             for(int count = number; count > 0; count--)
             {
+                //Stores the number to be factorialized as count as the original number needs to be used later
+                //For loop mulitplies numbers beginning with the starting number until it reaches zero
                 product = product * count;
             }
+            //Prints out the result of the factorial calculation
             System.out.println("The value of " + number + "! is " + product);
+            //Asks the user if they want to do another calculation
+            //Stores their answer as a string and converts it to lowercase so that the answer is case insentitive
             System.out.println("Would you like to calculate another number?");
             yesNo = kbInput.next();
             yesNoLower = yesNo.toLowerCase();
             if( yesNoLower == "yes")
             {
+                //If statement sets the boolean that repeats the do..while loop...
+                //if the answer is yes
+                //If the answer is anything other than yes, the boolean is set to false
                 anothernum = true;
             } else {
                 anothernum = false;
             } 
         }
-        while(anothernum == true);
+        while(anothernum == true); //If the user answered yes, the while statement repeats the entire calcualtion. If not, it ends the loop
+        //Prints an ending response to let the user know that the program has terminated.
         System.out.println("End Factorial Processing");
     }
 }
