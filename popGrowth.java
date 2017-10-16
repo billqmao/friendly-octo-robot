16b561ca19f18fb8faf08cfd80ac8323f2f4b2ea
billqmao <31422272+billqmao@users.noreply.github.com>
2017-10-16 11:41:49 -0400

    Version 1.51
    Fixing some file conflicts

--- popGrowth.java
+++ popGrowth.java
@@ -1,28 +1,24 @@
 /**
  * Name: Bill Mao
  *
- * Date: 10/11/17
+ * Date: 10/12/17
  *
  * School: East Chapel Hill High School
  *
  * Program: Program 162b (Population Growth)
  *
- * Description: This program is designed to calculate the sum, number of test scores, and average from a .dat file containing test scores.
- * Any variables needed outside of the scope of a specific loop are declared first.
- * The program then creates a scanner object that reads from the ini.dat file containing the test scores.
- * If the Java VM throws a FileNotFoundException, the program will catch that...
- * , and tell the user that they need to put the ini.dat file in the same folder as the program.
- * The program then prints the Scores table header.
- * A while loop then iterates until no more data is left in the .dat file.
- * In the loop, the individual test scores, which are integers, are retrieved by the fileInput scanner object,...
- * printed, added to the sum of the integers variable, and a count variable that stores the amount of integers in the file is incremented.
- * The average of all the integers is then calculated and rounded to two decimal places.
- * The results of the calculation are then printed for the user to see.
+ * Description: This program is designed to calculate when Westwalia's population will double based on a constant growth rate.
+ * The program first asks for and stores the growth rate of Westwalia.
+ * It then initializes three variables, the year, the multiplier used to calculate the population growth, and the year.
+ * An if statement then executes and terminates the program if the growth rate is 0 or negative.
+ * The two headers of the year and population are then printed.
+ * A while loop then runs until the population doubles.
+ * In the while loop, the next year's population is calculated along with the year, and is printed for the user to see.
+ * Once the population doubles, the year when it doubles is printed for the user to see.
+ * 
+ * Difficulties: I originally did population to the power of the multiplier instead of multiplying the two together.
  *
- * Difficulties: I had some difficulty finding how to iterate the loop until no more integers were left, but I managed to find the .hasNext method by looking at the provided examples.
- *
- * What I Learned: I learned how get integers from files on the computer, learned how to catch exceptions in Java, and how to iterate a loop as long as more objects/data is available.
- * I also got some more practice with the while loop.
+ * What I Learned: I got some more practice with the while loop, mathematical operations in Java, and casting.
  */
 
 import java.util.*;
@@ -31,85 +27,89 @@ import java.io.*;
 public class popGrowth{
     public static void main(String args[])
     {
-        Scanner kbInput = new Scanner(System.in);
-        System.out.println("Please enter in Westwalia's growth rate as a percentage");
+        Scanner kbInput = new Scanner(System.in); //Creates a new scanner object in order to get user input
+        //Asks user for Westwalia's growth rate and stores it as a double
+        System.out.println("Please enter in Westwalia's growth rate as a percentage"); 
         double growth = kbInput.nextDouble();
-        double multiplier = growth * .01 + 1;
-        int population = 236;
-        int year = 2010;
-        if(growth <= 0)
+        //The next three variables are declared so that they may be used outside of the scope of the loop
+        double multiplier = growth * .01 + 1; //Derived from the growth to use in the exponential population calculation
+        int population = 236; //Population of Westwalia
+        int year = 2010; //Starting year
+        if(growth <= 0) //If the growth rate the user entered is nonsensical (ie zero), the program will terminate.
         {
-            System.out.println("Please enter a valid growth rate");
+            System.out.println("Please enter a valid growth rate"); 
             System.out.println("The program will now exit");
             System.exit(0);
         }
+        //First two headers of the table are printed
         System.out.println("--------Growth Rate " + growth + "% annually--------");
-        System.out.println("Years  Population (in Millions)");
-        while(population < 472)
+        System.out.println("Years  Population (in Millions)"); 
+        while(population < 472) //Loop runs until population doubles
         {
-            year++;
-            population = (int) (Math.pow(population, multiplier) +.5);
-            System.out.println(year + "           " + population);
+            year++; //The year variable in incremented first
+            population = (int) (population * multiplier + .5); //The population growth for the year is then calculated and rounded to the nearest whole number
+            System.out.println(year + "           " + population); //The year and new population are then printed
         }
-        System.out.println("The population will double by " + year);
+        System.out.println("The population will double by " + year); //Prints out when the population will double
     }
 }
 //Output:
 //Please enter in Westwalia's growth rate as a percentage
-//1.7
-//--------Growth Rate 1.7% annually--------
+//5.9
+//--------Growth Rate 5.9% annually--------
 //Years  Population (in Millions)
-//2011           259
-//2012           285
-//2013           314
-//2014           346
-//2015           382
-//2016           423
-//2017           469
-//2018           521
-//The population will double by 2018
+//2011           250
+//2012           265
+//2013           281
+//2014           298
+//2015           316
+//2016           335
+//2017           355
+//2018           376
+//2019           398
+//2020           421
+//2021           446
+//2022           472
+//The population will double by 2022
 //
 //Please enter in Westwalia's growth rate as a percentage
-//.5
-//--------Growth Rate 0.5% annually--------
+//4.6
+//--------Growth Rate 4.6% annually--------
 //Years  Population (in Millions)
-//2011           243
-//2012           250
-//2013           257
-//2014           264
-//2015           271
-//2016           279
-//2017           287
-//2018           295
-//2019           304
-//2020           313
-//2021           322
-//2022           331
-//2023           341
-//2024           351
-//2025           361
-//2026           372
-//2027           383
-//2028           395
-//2029           407
-//2030           419
-//2031           432
-//2032           445
-//2033           459
-//2034           473
-//The population will double by 2034
+//2011           247
+//2012           258
+//2013           270
+//2014           282
+//2015           295
+//2016           309
+//2017           323
+//2018           338
+//2019           354
+//2020           370
+//2021           387
+//2022           405
+//2023           424
+//2024           444
+//2025           464
+//2026           485
+//The population will double by 2026
 //
 //Please enter in Westwalia's growth rate as a percentage
-//2.1
-//--------Growth Rate 2.1% annually--------
+//8.9
+//--------Growth Rate 8.9% annually--------
 //Years  Population (in Millions)
-//2011           265
-//2012           298
-//2013           336
-//2014           380
-//2015           430
-//2016           488
-//The population will double by 2016
+//2011           257
+//2012           280
+//2013           305
+//2014           332
+//2015           362
+//2016           394
+//2018           467
+//2019           509
+//The population will double by 2019
+
+
+
 
 
 
