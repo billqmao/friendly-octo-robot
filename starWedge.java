821818dfca530fbe38d5303f5694d4c4cda80616
billqmao <31422272+billqmao@users.noreply.github.com>
2017-10-18 21:38:45 -0400

    Version 1.55
    STUFFZ!

--- starWedge.java
+++ starWedge.java
@@ -2,18 +2,26 @@
 /**
  * Name: Bill Mao
  * 
- * Date: 9/5/2017
+ * Date: 10/18/2017
  * 
  * School: East Chapel Hill High School
  * 
- * Program: Hello World Program
+ * Program: Program165a (“wegie” of stars)
  * 
- * Description: This is the first program I have made in Java, and it is designed to print "Hello World!" to the terminal.
+ * Description: This program is designed to proint out a wedge of stars based on a starting number of rows and stars the user gives.
+ * It first asks the user for the starting number of rows and stars and stores it.
+ * The variables representing the row number and the number of stars are then initialized.
+ * A set of nested for loops then executes.
+ * The inner for loop is designed to print out a row of stars depending on the row number, which creates the wedge shape.
+ * For example, the topmost row will have the starting number of stars, the row below it one less star, and so on.
+ * The outer for loops iterates the rows from the starting number to 0.
+ * This results in the wedge having the starting number amount of rows.
+ * Once both loops are done printing, a wedge is created.
  * 
- * Difficulties: I encountered no difficulties creating this program.
+ * Difficulties: I originally forgot to print a filler string at the end of each column, so all the stars appeared on one line.
+ * I also had to switch some of the variable names around to make their purpose more clear
  * 
- * What I Learned: I leared how to print text strings to the terminal in Java, how to put comments in Java,
- * and the basic structure of most Java programs.
+ * What I Learned: I got some more practice with nested loops and formatting text.
  */
 
 import java.util.*;
@@ -23,18 +31,43 @@ public class starWedge
 {
     public static void main(String args[])
     {
-        Scanner kbInput = new Scanner(System.in);
+        Scanner kbInput = new Scanner(System.in); //Creates scanner object in order to get user input
+        //Asks for and stores starting column and row size
         System.out.println("Enter the starting size: ");
-        int stars = kbInput.nextInt();
-        int column = 0;
-        int row = 0;
-        for(column = stars; column >= 1; column--)
+        int startingNumber = kbInput.nextInt();
+        System.out.println(""); //Space is printed for formatting reasons.
+        //Two variables are declares so that they can be used outside of the context of the loops if needed
+        int row = 0; //Amount of rows that the stars occupy
+        int stars = 0; //Starting number of stars for the first row
+        for(row = startingNumber; row >= 1; row--) //Increments from the starting size of rows to the last row
         {
-            for(row = column; row >= 1; row--)
+            for(stars = row; stars >= 1; stars--) //Iterates for however many stars there should be per row 
             {
-                System.out.print("*");
+                System.out.print("*"); //Prints a star for each iteration
             }
-            System.out.println("");
+            System.out.println(""); //A new line is printd once all the stars are printed for one row to seperate the rows.
         }
     }
 }
+
+//Output:
+//Enter the starting size: 
+//8
+//
+//********
+//*******
+//******
+//*****
+//****
+//***
+//**
+//*
+//
+//Enter the starting size: 
+//5
+//
+//*****
+//****
+//***
+//**
+//*
