8ec759017563a1a97320181511101966a06e08fb
billqmao <31422272+billqmao@users.noreply.github.com>
2017-11-01 22:16:07 -0400

    Version 1.74
    WOHOO!

--- diceCount.java
+++ diceCount.java
@@ -2,25 +2,23 @@
 /**
  * Name: Bill Mao
  *
- * Date: 10/30/2017
+ * Date: 11/1//2017
  *
  * School: East Chapel Hill High School
  *
- * Program: Program 215b (faster than a speeding bullet)
+ * Program: Program 209b (throwing three dice)
  *
- * Description: This program is designed to calculate the maximum height of a bullet fired with a muzzle velocity of 960 feet per second
- * The program first begins by declaring three variables that will be used outside of the scope of the loops,
- * These are the seconds elapsed, the height of the bullet, and the height of the bullet 1 second before the current time.
- * A for loop then iterates through the first to sixtieth second the bullet is in the air.
- * The loop first stores the current height to the previous height, and then calculates the new height for the new time.
- * An if statement then executes that terminates the loop is the previous height is larger than or equal to the new height,,,,
- * indicating that the apex has been reached.
- * The program then finally prints out the time the bullet reached the maximum time and the bullet height.
+ * Description: This program is designed to find how many combinations of three dice add up to 9 or 10.
+ * The program first begins by initializing all variables needed in the program so that they can be used outside of the scope of the loops if needed.
+ * A series of three nested loops then iterate through all 216 possible permutations of the three dice.
+ * For each iteration, a variable signifying the amount of possible permutations of dice that lead to a sum of 9 is incremented if the sum of the dice is nine.
+ * This also happens for a sum of 10.
+ * Once all 216 permutations have been summed, the nested loops terminate, and the amount of permutations with a sum of nine and ten are printed.
  *
- * Difficulties: I first set the program to iterate for seconds 0-60 inclusive, but I realized that this arrangement would not work since...
- * there is no bullet height that corresponds to -1 seconds for the previous height.
+ * Difficulties: I originally was unsure of what loop to choose, but I settled on the for loop since there was a definite beginning and end...,
+ * and the for loop used the least amount of lines and was the most readable. 
  *
- * What I Learned: I learned how to implement decision making in programs using the if statement and the break statement.
+ * What I Learned: I got some more practice with nested loops and decision making using if statements.
  */
 
 import java.util.*;
@@ -31,33 +29,44 @@ public class diceCount
     public static void main(String args[])
     {
         //Variables needed outside of the scope of the loop are first initialized
-        int dice1 = 1; //Seconds the bullet has been in the air
-        int dice2 = 1; //Height of the bullet in feet
-        int dice3 = 1; //Previous height of the bullet before one second; used to find the greatest height
-        int countNine = 0;
-        int countTen = 0;
+        int dice1 = 1; //Value of the first dice
+        int dice2 = 1; //Value of the second dice
+        int dice3 = 1; //Value of the third dice
+        int countNine = 0; //How many times the dice add up to a value of nine
+        int countTen = 0; //How many times the dice add up to a value of ten
         for(dice1 = 1; dice1 <= 6; dice1++)
         {
+            //First outermost for loop cycles through all possible values of the first dice (1-6)
             for(dice2 = 1; dice2 <= 6; dice2++)
             {
+                //Second for loop cycles through all possible values of the second dice for all values of the first dice
                 for(dice3 = 1; dice3 <= 6; dice3++)
                 {
+                    //Final for loop cycles through all possible values of the third dice based on the values of the previous two dice
+                    //Ex: 1,1,1; 1,1,2;...1,2,1...
                     if((dice1 + dice2 + dice3) == 9)
                     {
+                        //If the dice add up to nine, the countNine variable is incremented
                         countNine++;
                     }
                     if((dice1 + dice2 + dice3) == 10)
                     {
+                        //If the dice add up to ten, the countTen variable is incremented
                         countTen++;
                     }
                 }
             }
         }
+        //Prints how many 9's and 10's there are
         System.out.println("There are " + countNine + " possible ways to get nine \nThere are " + countTen + " possible ways to get ten");
     }
 }
 //Output:
-//The maximum height reached by the bullet is 14400 feet after 30 seconds
+//There are 25 possible ways to get nine
+//There are 27 possible ways to get ten
+
+
+
 
 
 
