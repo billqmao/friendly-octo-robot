15eec0569fad468aaead37bcad35d9d8e17aecaa
billqmao <31422272+billqmao@users.noreply.github.com>
2017-11-15 22:41:26 -0500

    Version 1.92
    IM HOLDING ON

--- CricketTemp.java
+++ CricketTemp.java
@@ -1,19 +1,23 @@
-
 /**
  * Name: Bill Mao
- * 
- * Date: 9/5/2017
- * 
+ *
+ * Date: 11/15/2017
+ *
  * School: East Chapel Hill High School
- * 
- * Program: Hello World Program
- * 
- * Description: This is the first program I have made in Java, and it is designed to print "Hello World!" to the terminal.
- * 
- * Difficulties: I encountered no difficulties creating this program.
- * 
- * What I Learned: I leared how to print text strings to the terminal in Java, how to put comments in Java,
- * and the basic structure of most Java programs.
+ *
+ * Program: Program 213d (Weather Forecasting)
+ *
+ * Description: This program is designed to find the temperature of the surrounding environment based on the amount of cricket chirps there are.
+ * Variables used later in the program are first declared for clarity purposes.
+ * The user is then asked for the amount of chirps in the environment.
+ * The amount of chirps is the  converted to degrees fahrenheit, which itself is converted to degrees celsius.
+ * The temperature in both celsius and fahrenheit is then printed for the user to see.
+ * Finally, an if else decision tree prints a statement related to the severity of the temperature based on the temperature in degrees fahrenheit.
+ *
+ * Difficulties: I originally got a temperature of 0 celsius for all chirps, as I did not use doubles in the conversion ratio from fahrenheit to celsius.
+ * Ex: (fahrenheit - 32) * (5 / 9) = (fahrenheit - 32) * 0 instead of (fahrenheit - 32) * (5.0 / 9.0)
+ *
+ * What I Learned: I got some more practice with mathematical calculations, primitive data types, and if else decision trees, and
  */
 
 import java.util.*;
@@ -23,18 +27,77 @@ public class CricketTemp
 {
     public static void main(String args[])
     {
-        Scanner kbInput = new Scanner(System.in);
-        int chirps = 0;
-        double fahrenheit = 0;
-        double celcius = 0;
+        //Variables used later in the program are first declared for clarity purposes
+        Scanner kbInput = new Scanner(System.in); //Used to get user input
+        int chirps = 0; //Cricket chirps per minute
+        double fahrenheit = 0; //Temperature in fahrenheit
+        double celsius = 0; //Temperature in celsius
+        //Asks the user for the amount of cricket chirps per minute
         System.out.println("Enter Cricket Chirps/Min:");
-        chirps = kbInput.nextInt();
+        chirps = kbInput.nextInt(); //Stores the chirps
+        //The temperature of the environment is found in fahrenheit
         fahrenheit = (chirps + 40) / 4;
-        celcius = (fahrenheit - 32) * (5 / 9);
+        //The temperature in celsius is then found and is  rounded to two decimal points
+        celsius = (fahrenheit - 32) * (5.0 / 9.0);
+        celsius = (int) (celsius * 100 + .5) / 100.0;
+        //Prints the temperatures
         System.out.println("Fahrenheit = " + fahrenheit);
-        System.out.println("Celcius = " + celcius);
-        
-
-    } 
+        System.out.println("Celsius = " + celsius);
+        //Temperature in fahrenheit is used to print the severity of the temperature
+        if(fahrenheit >= 109)
+        {
+            //Severity of the temperature at 109 and greater degrees fahrenheit
+            System.out.println("Dead Cricket");
+        }
+        else if(fahrenheit >= 81 && fahrenheit <= 108)
+        {
+            //Severity of the temperature from 81 to 108 degrees fahrenheit
+            System.out.println("Whew!");
+        }
+        else if(fahrenheit >= 65 && fahrenheit <= 80)
+        {
+            //Severity of the temperature from 65 to 80 degrees fahrenheit
+            System.out.println("Ah-hhh");
+        }
+        else if(fahrenheit >= 45 && fahrenheit <= 64)
+        {
+            //Severity of the temperature from 45 to 64 degrees fahrenheit
+            System.out.println("Take a jacket");
+        }
+        else if(fahrenheit < 45)
+        {
+           //Severity of the temperature at 44 and below degrees fahrenheit
+            System.out.println("Bur-rrr");
+        }
+    }
 }
-
+//Output:
+//Enter Cricket Chirps/Min:
+//20
+//Fahrenheit = 15.0
+//Celsius = -9.43
+//Bur-rrr
+//
+//Enter Cricket Chirps/Min:
+//180
+//Fahrenheit = 55.0
+//Celsius = 12.78
+//Take a jacket
+//
+//Enter Cricket Chirps/Min:
+//240
+//Fahrenheit = 70.0
+//Celsius = 21.11
+//Ah-hhh
+//
+//Enter Cricket Chirps/Min:
+//360
+//Fahrenheit = 100.0
+//Celsius = 37.78
+//Whew!
+//
+//Enter Cricket Chirps/Min:
+//480
+//Fahrenheit = 130.0
+//Celsius = 54.44
+//Dead Cricket
--- classifyStudent.java
+++ classifyStudent.java
@@ -1,19 +1,24 @@
-
 /**
  * Name: Bill Mao
- * 
+ *
  * Date: 11/14/2017
- * 
+ *
  * School: East Chapel Hill High School
- * 
+ *
  * Program: Program 213c (if then student classifier)
- * 
- * Description: This is the first program I have made in Java, and it is designed to print "Hello World!" to the terminal.
- * 
- * Difficulties: I encountered no difficulties creating this program.
- * 
- * What I Learned: I leared how to print text strings to the terminal in Java, how to put comments in Java,
- * and the basic structure of most Java programs.
+ *
+ * Description: This program is designed to classify a college student based on the amount of credits they have.
+ * The program first initializes any variables needed later in the program for clarity purposes.
+ * A do..while loop then executes.
+ * In the loop, the user is asked for the college student's ID number, and the amount of credits they have.
+ * The amount of credits is then converted to a grade code via an if else decision tree.
+ * The grade code is then stored and printed.
+ * Finally, the program asks if the user if they want to repeat the classification, and repeats the loop if they answer yes.
+ *
+ * Difficulties: I originally manually printed out the grade code by including it in the string and did not store it as a variable,...
+ * but I realised that it would be easier to add other possible grade code options or to store the grade codes permanently if I saved them to a variable.
+ *
+ * What I Learned: I got some more practice with the Scanner class in Java, if else decision trees, and the do..while loop.
  */
 
 import java.util.*;
@@ -23,41 +28,79 @@ public class classifyStudent
 {
     public static void main(String args[])
     {
-        String repeat = "yes";
-        int studentID = 0;
-        int gradeCode = 0;
-        int credits = 0;
-        Scanner kbInput = new Scanner(System.in);
-        do
+        //Variables used later in the program are first declared for clarity purposes
+        String repeat = "yes"; //used to see if the user wants to repeat the student classification
+        int studentID = 0; //Student ID of the person to be classified
+        int gradeCode = 0; //Code corresponding to the grade of the student
+        int credits = 0; //Credits the student has earned
+        Scanner kbInput = new Scanner(System.in); //Scanner for user input
+        do //Entire do..while loop allows for the classification to repeat
         {
-            System.out.println("Enter Student Number :");
-            studentID = kbInput.nextInt();
-            System.out.println("Enter Credits:");
+            System.out.println("Enter Student Number :"); //Asks for the student number...
+            studentID = kbInput.nextInt(); //and stores it
+            System.out.println("Enter Credits:"); //Same is done for the amount of credits
             credits = kbInput.nextInt();
-            if(credits >= 0 && credits < 30)
+            if(credits >= 0 && credits < 30) //The grade code is then determined by how many credits the student has
             {
-                System.out.println("Grade level code = 1");
+                gradeCode = 1; //Prints a grade code 1 for a freshman with [0,30) (interval notation) credits
+                System.out.println("Grade level code = " + gradeCode);
             }
             else if(credits >= 30 && credits < 70)
             {
-                System.out.println("Grade level code = 2");
+                gradeCode = 2; //Prints a grade code 2 for a sophomore with [30,70) (interval notation) credits
+                System.out.println("Grade level code = " + gradeCode);
             }
             else if(credits >= 70 && credits < 90)
             {
-                System.out.println("Grade level code = 3");
+                gradeCode = 3; //Prints a grade code 3 for a junior with [70,90) (interval notation) credits
+                System.out.println("Grade level code = " + gradeCode);
             }
             else if(credits >= 90)
             {
-                System.out.println("Grade level code = 4");
+                gradeCode = 4; //Prints a grade code 4 for a senior with [90,infinity) (interval notation) credits
+                System.out.println("Grade level code = " + gradeCode);
             }
             else
             {
+                //If the user enters a nonsensical amount of credits (negative), an error message is printed
                 System.out.println("Please enter a positive integer for the number of credits.");
             }
-            System.out.println("Do you wish to find another student? (yes/no)");
-            repeat = kbInput.next();
+            System.out.println("Do you wish to find another student? (yes/no)"); //Asks the user if they wish to repeat the classification
+            repeat = kbInput.next(); //Stores the user's response
+            System.out.println(""); //New line for formatting purposes
         }
-        while(repeat.equalsIgnoreCase("yes"));
-    } 
+        while(repeat.equalsIgnoreCase("yes")); //Loop repeats if the user answers yes
+    }
 }
-
+//Output:
+//Enter Student Number :
+//4467
+//Enter Credits:
+//30
+//Grade level code = 2
+//Do you wish to find another student? (yes/no)
+//yes
+//
+//Enter Student Number :
+//5544
+//Enter Credits:
+//15
+//Grade level code = 1
+//Do you wish to find another student? (yes/no)
+//yes
+//
+//Enter Student Number :
+//6846
+//Enter Credits:
+//70
+//Grade level code = 3
+//Do you wish to find another student? (yes/no)
+//yes
+//
+//Enter Student Number :
+//65782
+//Enter Credits:
+//90
+//Grade level code = 4
+//Do you wish to find another student? (yes/no)
+//no
